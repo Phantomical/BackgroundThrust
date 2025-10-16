d201a667a3740d5b518683ce622d6a0b3365b2af
Phantomical <phantom@lynches.ca>
2025-10-16 02:18:11 -0700

    Allow timewarping under acceleration when throttle is non-zero

--- src/BackgroundThrust/Patches/TimeWarp_Patch.cs
+++ src/BackgroundThrust/Patches/TimeWarp_Patch.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System.Reflection.Emit;
-using BackgroundThrust.Utils;
 using HarmonyLib;
 
 namespace BackgroundThrust.Patches;
@@ -39,4 +38,34 @@ internal static class TimeWarp_SetRate_Patch
 
         return InputLockManager.SetControlLock(controlTypes & THROTTLE_MASK, lockId);
     }
+}
+
+[HarmonyPatch(typeof(TimeWarp), "getMaxOnRailsRateIdx")]
+internal static class TimeWarp_GetMaxOnRailsRateIdx_Patch
+{
+    static bool ShouldAllowWarpWhileAccelerating(TimeWarp timeWarp)
+    {
+        var vessel = FlightGlobals.ActiveVessel;
+        if (vessel == null)
+            return false;
+
+        if (timeWarp.current_rate_index > timeWarp.maxPhysicsRate_index)
+            return true;
+
+        var module = vessel.FindVesselModuleImplementing<BackgroundThrustVessel>();
+        if (module.Throttle != 0.0)
+            return true;
+
+        return false;
+    }
+
+    static void Prefix(TimeWarp __instance, out double __state)
+    {
+        __state = TimeWarp.GThreshold;
+
+        if (ShouldAllowWarpWhileAccelerating(__instance))
+            TimeWarp.GThreshold = double.PositiveInfinity;
+    }
+
+    static void Finalizer(double __state) => TimeWarp.GThreshold = __state;
 }
